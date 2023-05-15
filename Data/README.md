37d4da4cadcdbecc2416a8740749bbc031f16b2e
SufurElite <13058966+SufurElite@users.noreply.github.com>
2023-05-15 04:38:51 +0100

    Fixed the results to align with the paperspace environment

--- Data/README.md
+++ Data/README.md
@@ -3,7 +3,6 @@
 ## Custom Dataset texts Included:
 ### The Latin Library
 https://github.com/cltk/lat_text_latin_library
-~108 MB
 
 ### Italian Poets in Latin
 https://www.poetiditalia.it/public/
--- LatinBERT/predict_words.py
+++ LatinBERT/predict_words.py
@@ -68,17 +68,21 @@ def infilling(wp_tokenizer, text_before_pred, text_after_lacuna, model):
 		total_text = text_before_pred + " " + predicted_word + " " + text_after_lacuna
 	
 	return total_text
+
 		
-def predict(wp_tokenizer, text_before_pred, model):
+def	predict(wp_tokenizer, text_before_pred,	model,	context_size:int=256,	k:int	=	20,	sampleK:bool	=	False):
 
 	tokens=[]
 	tokens.extend(wp_tokenizer.tokenize(text_before_pred))
-	position=len(tokens) + 1
+	position=len(tokens)	+	1
 	tokens.append("[MASK]")
+	pads = context_size-len(tokens)-2
+	tokens+=["[PAD]"]*pads
 	#tokens.extend(wp_tokenizer.tokenize(text_after_lacuna))
 
-	#tokens.insert(0,"[CLS]")
-	#tokens.append("[SEP]")
+	tokens.insert(0,"[CLS]")
+	tokens.append("[SEP]")
+	#print(tokens)
 
 	tokenids=wp_tokenizer.convert_tokens_to_ids(tokens)	
 
@@ -86,41 +90,45 @@ def predict(wp_tokenizer, text_before_pred, model):
 
 	torch_tokenids=torch.LongTensor(tokenids).unsqueeze(0)
 	torch_tokenids=torch_tokenids.to(device)
-	total_text = ""
+	total_text	= ""
 	with torch.no_grad():
-		preds = model(torch_tokenids)
-		kth_vals, kth_idx = preds.topk(40, dim=-1)
-		dist = torch.distributions.categorical.Categorical(logits=kth_vals)
-		idx = kth_idx.gather(dim=1, index=dist.sample().unsqueeze(-1)).squeeze(-1)
-	
+		preds =	model(torch_tokenids)
+		preds =	preds[0]
+		
+		sortedVals=torch.argsort(preds[0][mask_id],	descending=True)
+		idx	= 0
+		if	sampleK:
+			idx	= random.randint(0,k)
+			
+		p = sortedVals[idx]
 		predicted_index=p.item()
 		probs=nn.Softmax(dim=0)(preds[0][mask_id])
 
 
 		suffix=""
-		if not wp_tokenizer.reverseVocab[predicted_index].endswith("_"):
+		if	not	wp_tokenizer.reverseVocab[predicted_index].endswith("_"):
 			uptokens=copy.deepcopy(tokenids)
-			uptokens.insert(position, predicted_index)
-			suffix=proc(uptokens, wp_tokenizer, model)
+			uptokens.insert(position,	predicted_index)
+			suffix=proc(uptokens,	wp_tokenizer,	model)
 		
-		predicted_word="%s%s" % (wp_tokenizer.reverseVocab[predicted_index], suffix)
-		predicted_word=re.sub("_$", "", predicted_word).lower()
+		predicted_word="%s%s"	%	(wp_tokenizer.reverseVocab[predicted_index],	suffix)
+		predicted_word=re.sub("_$",	"",	predicted_word).lower()
 		total_text = text_before_pred + " " + predicted_word
 	
-	return total_text
+	return	total_text
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-b', '--bertPath', help='path to pre-trained BERT', required=True)
-	parser.add_argument('-t', '--tokenizerPath', help='path to Latin WordPiece tokenizer', required=True)
+	parser.add_argument('-b', '--bertPath',	help='path	to	pre-trained	BERT',	required=True)
+	parser.add_argument('-t', '--tokenizerPath', help='path	to	Latin	WordPiece	tokenizer',	required=True)
 	
 	args = vars(parser.parse_args())
 
 	bertPath=args["bertPath"]
 	tokenizerPath=args["tokenizerPath"]			
 	
-	encoder = text_encoder.SubwordTextEncoder(tokenizerPath)
+	encoder	= text_encoder.SubwordTextEncoder(tokenizerPath)
 	wp_tokenizer = LatinTokenizer(encoder,lowercase=True)
 
 	model = BertForMaskedLM.from_pretrained(bertPath)
